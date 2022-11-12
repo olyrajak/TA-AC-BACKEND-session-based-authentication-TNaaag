f0b613549bf4fa3c604e6a84692112158a4aa37c
Oly Rajak <59551153+olyrajak@users.noreply.github.com>
2022-11-12 11:30:29 +0530

    Update app.js

--- block-BNaaev/authbycryptregistration/app.js
+++ block-BNaaev/authbycryptregistration/app.js
@@ -30,7 +30,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 app.use('/register', registerRouter);
+
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
