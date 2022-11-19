3d1930e730cc67d7dcca39c9ff0622532b32ac34
Oly Rajak <59551153+olyrajak@users.noreply.github.com>
2022-11-19 23:56:01 +0530

    Update app.js

--- block-BNaadt/FlashMessageAndLogout/app.js
+++ block-BNaadt/FlashMessageAndLogout/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
 var indexRouter = require('./routes/index');
 var registerRouter = require('./routes/register');
 var loginRouter = require('./routes/login');
@@ -25,6 +26,8 @@ mongoose.connect("mongodb://localhost/UserLogin", {
     }
 });
 // view engine setup
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -54,7 +57,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
+    // set locals, only providing error
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
